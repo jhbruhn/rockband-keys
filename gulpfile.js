bc512857e2e4ad5af6846de761b6492360056ea2
Jan-Henrik <jh.bruhn@me.com>
2014-05-13 14:58:21 +0200

    Added readme

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var jsdoc = require("gulp-jsdoc");
 
 gulp.task('docs', function() {
 
-  gulp.src("./lib/*.js")
+  gulp.src(["./README.md", "./lib/*.js"])
     .pipe(jsdoc.parser({}))
     .pipe(jsdoc.generator('./doc'))
 });
